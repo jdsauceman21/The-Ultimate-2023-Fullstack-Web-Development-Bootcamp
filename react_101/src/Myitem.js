37a122f106c8aaea2d590966ebe5b4d0d2933469
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-09-21 15:43:42 -0400

    added click event

--- react_101/src/Myitem.js
+++ react_101/src/Myitem.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
 class Item extends React.Component {
+
+    clickMe() {
+        console.log("I was clicked")
+    }
     render() {
       return(
-        <h1>Hello from {this.props.name}</h1>
+        <h1 onClick={() => this.clickMe()}>Hello from {this.props.name}</h1>
       )
     }
 }
