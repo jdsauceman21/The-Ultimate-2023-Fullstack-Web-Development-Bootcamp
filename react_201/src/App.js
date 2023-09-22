3f77d3b87f5ef9b08dcff42ebed8540573f94440
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-09-22 15:58:18 -0400

    created react 201 and added navbar

---
+++ react_201/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <h1>hello from React 201</h1>
+    </div>
+  );
+}
+
+export default App;
