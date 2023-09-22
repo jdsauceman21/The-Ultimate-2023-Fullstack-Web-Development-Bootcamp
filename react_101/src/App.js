0afe1c4ac270e5ddde3280b97d7bd295bafe5a23
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-09-22 14:38:52 -0400

    finished react 101

--- react_101/src/App.js
+++ react_101/src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Item from './Myitem';
+import React from 'react';
+
+const logo = "https://upload.wikimedia.org/wikipedia/commons/6/6c/Star_Wars_Logo.svg"
+const apiUrl = "https://rawcdn.githack.com/akabab/starwars-api/0.2.1/api/id"
+
+class FilmItemRow extends React.Component {
+  render() {
+    return (
+      <li>
+        <a href={this.props.url}>{this.props.url}</a>
+      </li>
+    );
+  }
+}
+
+class StarWarsTitle extends React.Component {
+  render() {
+    return (
+      <>
+        <img src={logo} className="App-logo" alt="StarWars" />
+      </>
+    )
+  }
+}
+
+class StarWars extends React.Component {
+  constructor(){
+    super()
+    this.state ={
+      loadedCharacter: false,
+      name: null,
+      image: null,
+      height: null,
+      homeworld: null,
+      // films: [],
+    }
+  }
+  getNewCharacter() {
+    const rand = Math.round( Math.random() * 88)
+    // const url = `https://github.com/akabab/starwars-api/tree/master/api/id/${rand}`;
+    const url = `${apiUrl}/${rand}.json`;
+    fetch(url)
+    .then(Response => Response.json())
+    .then(data =>{
+      this.setState({
+        name: data.name,
+        image: data.image,
+        height: data.height,
+        homeworld: data.homeworld,
+        // films: [data.films],
+        loadedCharacter: true,
+      })
+    })
+    
+  }
+  render() {
+
+    // const movies = this.state.films.map((url, i) => {
+    //   return <FilmItemRow key={i} url={url} />
+    // })
+
+    return(
+      <div>
+        {
+          this.state.loadedCharacter &&
+          <div>
+            <h1>{this.state.name}</h1>
+            <img src={this.state.image} alt= {this.state.image} height="300" />
+            <p>Height: {this.state.height} m</p>
+            <p>Home World: <span className="capitalize">{this.state.homeworld}</span></p>
+            {/* <ul>{movies}</ul> */}
+          </div>
+        }
+        <button type='button' onClick={() => this.getNewCharacter()} 
+        className='btn'>Randomize Character</button>
+      </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Item name="South Carolina" />
-        <Item name="Tennessee" />
-        <Item name="Alabama" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <StarWarsTitle />
+        <StarWars />
       </header>
     </div>
   );
--- react_101/src/Myitem.js
+++ react_101/src/Myitem.js
@@ -2,12 +2,27 @@ import React from 'react';
 
 class Item extends React.Component {
 
+    constructor(props) {
+      super(props)
+
+      this.state = {
+        clicks: 0,
+        totalRemaining: 100,
+      }
+    }
+
     clickMe() {
-        console.log("I clicked:", this.props.name)
+        this.setState({
+          clicks: this.state.clicks + 1,
+          totalRemaining: this.state.totalRemaining - 1
+        })
     }
     render() {
       return(
-        <h1 onClick={() => this.clickMe()}>Hello from {this.props.name}</h1>
+        <div>
+          <h1 onClick={() => this.clickMe()}>Hello from {this.props.name}</h1>
+          <span>{this.state.clicks} is the number of clicks. {this.state.totalRemaining} is the Total Remaining</span>
+        </div>
       )
     }
 }
