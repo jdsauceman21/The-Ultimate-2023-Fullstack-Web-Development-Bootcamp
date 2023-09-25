07814d6313718445c7ec339b3b1e72d30db2b7f4
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-09-25 08:58:41 -0400

    added about

--- react_201/src/App.js
+++ react_201/src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import AboutView from './components/AboutView';
+import {Switch,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Switch>
+        <Route path= "/" exact/>
+        <Home/>
+        <Route path="/about" component={AboutView} />
+      </Switch>
+      <AboutView/>
     </div>
   );
 }
