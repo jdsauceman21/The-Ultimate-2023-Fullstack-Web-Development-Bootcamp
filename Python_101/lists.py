5d5038f1c44acc56462d6877dde0f08e7c69b80c
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-10-04 14:19:08 -0400

    un comminted

--- Python_101/lists.py
+++ Python_101/lists.py
@@ -1,4 +1,4 @@
-lst = ["String", 1, 3.14, ["A new item"], "Kalob"]
+lst = ["String", 1, 3.14, ["A new item"], "Jarrett"]
 
-# for item in lst:
-#     print("The item is:", item)
+for item in lst:
+    print("The item is:", item)
