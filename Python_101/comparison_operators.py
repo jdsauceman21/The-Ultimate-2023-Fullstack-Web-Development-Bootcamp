b8dd9531ac2df77821ed9c2adaefba76b75a976b
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-10-05 07:31:31 -0400

    small changes

--- Python_101/comparison_operators.py
+++ Python_101/comparison_operators.py
@@ -14,34 +14,27 @@
 # else:
 #     print("You are a student. Welcome to Python 101")
 
-# name = input("What is your name? ")
-# if name == "Bob":
-#     print("Welcome Bob!")
-#     bring_food = "Pizza"
-# elif name == "Kalob":
-#     print("Welcome to your teacher portal")
-#     bring_food = "Tacos"
-# elif name == "Nathan":
-#     print("Welcome to the Gym")
-#     bring_food = "Weigh protein"
-# else:
-#     print("You're not bob get outta here")
-#     bring_food = "Salmon"
+name = input("What is your name? ")
+if name == "Bob":
+    print("Welcome Bob!")
+    bring_food = "Pizza"
+elif name == "Jarrett":
+    print("Welcome to your teacher portal")
+    bring_food = "Tacos"
+elif name == "Amanda":
+    print("Welcome to the Gym")
+    bring_food = "Weigh protein"
+else:
+    print("You're not bob get outta here")
+    bring_food = "Salmon"
 
-# print(f"You are eating {bring_food}")
+print(f"You are eating {bring_food}")
 
-# name = input("What is your name? ")
-# name = name.lower()
+name = input("What is your name? ")
+name = name.lower()
 
-# if name != "bob":
-#     print("You're not bob, get out of here")
-# else:
-#     print("Welcome Bobby boy")
-
->
->=
-<
-<=
-== is the same
-!= is not the same
+if name != "bob":
+    print("You're not bob, get out of here")
+else:
+    print("Welcome Bobby boy")
 
--- Python_101/print_formatting.py
+++ Python_101/print_formatting.py
@@ -1,4 +1,4 @@
-name = "Kalob"
+name = "Jarrett"
 # welcome_message = "Hello {} welcome to Python 101".format(name)
 welcome_message = f"Hello {name} welcome to Python 101"
 
