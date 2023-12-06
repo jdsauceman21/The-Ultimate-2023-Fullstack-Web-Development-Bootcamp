3b6fe27282fbed0b6085c0a9ebb97622410c6c8d
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-12-06 09:27:53 -0500

    installed pip and other things

--- Python_201/list_comprehensions.py
+++ Python_201/list_comprehensions.py
@@ -3,6 +3,6 @@ for num in [1, 3, 5, 7, 9]:
     numbers.append(num**2)
 print(numbers)
 
-numbers = [num**2 for num in [1, 3, 5, 7, 9]]
+numbers = [num**3 for num in [1, 3, 5, 7, 9]]
 print(numbers)
 
