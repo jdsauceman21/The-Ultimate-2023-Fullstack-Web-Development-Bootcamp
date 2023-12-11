3b6b979839b8e475783ba3cb866714973af9cd54
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-12-11 15:59:32 -0500

    finnished lesson

--- Django_101/mysite/urls.py
+++ Django_101/mysite/urls.py
@@ -14,6 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from feed import urls as feed_urls
@@ -23,3 +26,5 @@ urlpatterns = [
     path('', include(feed_urls, namespace='feed')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
