7f22b6f530dc6bb31c5923d075ca9e6a0f167869
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-12-11 15:48:45 -0500

    edited models

--- Django_101/feed/models.py
+++ Django_101/feed/models.py
@@ -3,7 +3,7 @@ from sorl.thumbnail import ImageField
 
 class Post(models.Model):
     text = models.CharField(max_length=140, blank=False, null=False)
-    image = ImageField(default='path/to/default/image.jpg')  # Set a default image path
+    image = ImageField()
 
     def __str__(self):
         return self.text
