555029edf74ca72104c8b990ffd42bcb83e60247
Jarrett D Sauceman <89327514+jdsauceman21@users.noreply.github.com>
2023-12-13 15:44:57 -0500

    added Detail view

--- Django_101/feed/views.py
+++ Django_101/feed/views.py
@@ -1,12 +1,16 @@
 from typing import Any
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from .models import Post
 
 
 class HomePageView(TemplateView):
-    template_name = "Home.html"
+    template_name = "home.html"
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context ['posts'] = Post.objects.all()
         return context
+    
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
